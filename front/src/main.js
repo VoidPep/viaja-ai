1b01748956884a5d93eed6f4e7b26fd0635a32d7
voidpep <pedrohm1009@gmail.com>
2024-09-18 23:09:53 -0300

    att front packages

--- front/src/main.js
+++ front/src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 import 'primeflex/primeflex.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 const app = createApp(App);
 app.use(PrimeVue, {
