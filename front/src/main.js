653aa152cdd38d76dd8d7266f958fd19c3df39c3
voidpep <pedrohm1009@gmail.com>
2024-09-18 23:05:20 -0300

    import primeflex

--- front/src/main.js
+++ front/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import 'primeflex/primeflex.css';
 
 const app = createApp(App);
 app.use(PrimeVue, {
