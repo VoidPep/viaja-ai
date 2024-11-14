3c91bf843de069fd7c99573ee32c83fcb56533e3
Estevan <este2662@gmail.com>
2024-11-14 00:15:41 -0300

    mensagem

--- back/src/modules/roteiros/entities/roteiro.entity.ts
+++ back/src/modules/roteiros/entities/roteiro.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
-import { Usuario } from "src/modules/user/entity/user.entity";
+import { Usuario } from '../../user/entity/user.entity';
+
 @Entity()
 export class Roteiro {
     @PrimaryGeneratedColumn()
