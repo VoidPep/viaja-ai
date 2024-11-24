4b4c16fc796d76b52eef0d94bd2e13b8cd2a52df
voidpep <pedrohm1009@gmail.com>
2024-11-23 21:35:09 -0300

    Adição imagens no roteiro

--- back/src/services/roteiros/roteiros.service.ts
+++ back/src/services/roteiros/roteiros.service.ts
@@ -1,30 +1,56 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { CreateRoteiroDto } from '../../modules/roteiros/dto/create-roteiro.dto';
 import { Roteiro } from '../../modules/roteiros/entity/roteiro.entity';
 import { DataSource, Repository } from 'typeorm';
+import { createApi, Language } from "unsplash-js";
+import { PromptRequest } from 'src/modules/gemini/gemini.request';
+import { ImagemDoRoteiro } from 'src/modules/roteiros/entity/imagem-do-roteiro.entity';
 
 @Injectable()
 export class RoteirosService {
-  repository: Repository<Roteiro>;
+  roteiroRepository: Repository<Roteiro>;
   constructor(
     @Inject("DATA_SOURCE") private readonly database: DataSource,
   ) {
-    this.repository = this.database.getRepository(Roteiro);
+    this.roteiroRepository = this.database.getRepository(Roteiro);
+  }
+
+  async criarImagens(roteiro: any, request: PromptRequest) {
+    const api = createApi({
+      accessKey: "Client-ID qdp3gIWjG86yEIksq7zyneUSxEEh7sv-Ypzg1eo7Jko"
+    });
+
+    const {response} = await api.search.getPhotos({ 
+        query: `
+          ${roteiro.destino} 
+          ${request.preferencias.join(', ').replace('_', ' ').toLowerCase()}
+        `,
+        orientation: 'landscape',
+        lang: Language.Portuguese,
+        perPage: 3
+     })
+
+     let links = response.results.map(r => r.links.html)
+     let imagens: ImagemDoRoteiro[] = [];
+     links.forEach(link => {
+      imagens.push({ url: link } as ImagemDoRoteiro)
+     })
+
+     return imagens
   }
 
   async getByLoggedUser(id: any) {
-    return await this.repository.find({
+    return await this.roteiroRepository.find({
       where: {
         usuario: { id: id },
       },
       relations: ['usuario'],
     });
   }
-  
+
   async create(roteiro: any) {
-    return await this.repository.save(roteiro)
+    return await this.roteiroRepository.save(roteiro)
   }
   async remove(id: number) {
-    return await this.repository.delete(id)
+    return await this.roteiroRepository.delete(id)
   }
 }
