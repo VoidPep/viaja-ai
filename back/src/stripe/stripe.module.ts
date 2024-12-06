179fab366dac06433b134eb863fd2d05846ed4e1
voidpep <pedrohm1009@gmail.com>
2024-12-05 22:35:52 -0300

    correção modulo stripe

--- back/src/stripe/stripe.module.ts
+++ back/src/stripe/stripe.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { StripeController } from './stripe.controller';
 import { StripeService } from './stripe.service';
 import { UsuarioService } from 'src/services/account/user.service';
+import { UsuarioModule } from 'src/modules/user/user.module';
 
 @Module({
+  imports: [UsuarioModule],
   controllers: [StripeController],
-  providers: [StripeService, UsuarioService],
+  providers: [StripeService],
 })
 export class StripeModule {}
